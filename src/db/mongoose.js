cc5a36c99ca32c8c4cca1a29b57b5c0de57aa334
Hamzat Azeez <azeezhamzy99@gmail.com>
2022-03-31 22:36:41 +0100

    Changes Made

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
